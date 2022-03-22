2821130605a8163345e3d64384de9c2c4b9c1879
Yolandaokyere <yolandaokyere@outlook.com>
2022-03-22 12:48:33 +0100

    npm installaties

--- RijksArt-progressiveWeb/index.js
+++ RijksArt-progressiveWeb/index.js
@@ -1,4 +1,13 @@
-console.log('test!')
+require('dotenv').config()
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+
+const PORT = 7777
 
 const { format } = require('date-fns');
 const { required } = require('nodemon/lib/config');
