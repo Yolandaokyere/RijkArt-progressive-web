f3acf0b652d069a282e3a9c25fb68d45e9b634a3
Yolandaokyere <yolandaokyere@outlook.com>
2022-04-06 12:41:50 +0200

    cash method unsuccessful

---
+++ RijksArt-progressiveWeb/public/service-worker.js
@@ -0,0 +1,52 @@
+// Caching the pages
+const cacheName = "version-2";
+const contentToCache = [
+  // array of all the pages that I want to cashe
+  "/",
+  "index.ejs",
+  "style.css",
+  "main.js",
+];
+// Call Install event
+self.addEventListener("install", (e) => {
+  console.log("Service worker: Installed");
+
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("Service Worker: Caching Files(cacheAssets)");
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+// Call Activate event
+self.addEventListener("activate", (e) => {
+  console.log("Service worker: Activated");
+  // cleaning up old cache
+  e.waitUntil(
+    cashes.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Service worker: Cleaning old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// // Call Fetch event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(() => {
+      return fetch(e.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
